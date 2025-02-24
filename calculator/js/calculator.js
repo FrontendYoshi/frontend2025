018cff295ab788765f927f300a78619311ed98b4
fullstack <noahneweling@outlook.de>
2025-02-24 21:41:36 +0100

    calculator

---
+++ calculator/js/calculator.js
@@ -0,0 +1,17 @@
+// Funktion Addition zweier Zahlen
+function add(a, b) {
+    return a + b;
+}
+// Funktion Substraktion zweier Zahlen
+function substract (a, b) {
+    return a - b;
+}
+// Funktion Multiplikation zweier Zahlen
+function multiply(a, b) {
+    return a * b;
+}
+// Funktion Division zweier Zahlen
+function divide(a, b) {
+    return a / b;
+}
+
